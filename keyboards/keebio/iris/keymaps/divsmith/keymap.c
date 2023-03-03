a8fa537ff546eb02de32bf992bf6d44dd4c7d891
Parker Smith <parker@parkersmith.us>
2023-03-03 15:19:31 -0700

    Home row modifier tinkering

--- keyboards/keebio/iris/keymaps/divsmith/keymap.c
+++ keyboards/keebio/iris/keymaps/divsmith/keymap.c
@@ -26,6 +26,7 @@ tap_dance_action_t tap_dance_actions[] = {
    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
