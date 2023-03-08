57fb686b3f7ef15974cc9b504419b7fe2ff48b5c
Parker Smith <parker@parkersmith.us>
2023-03-08 12:37:37 -0700

    Add back control keys

--- keyboards/keebio/iris/keymaps/divsmith/keymap.c
+++ keyboards/keebio/iris/keymaps/divsmith/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC, LSFT_T(KC_A), LT(SYMBOL, KC_S),    KC_D,    KC_F,   KC_G,                KC_H,    KC_J,    KC_K,    KC_L,    RSFT_T(KC_SCLN), KC_QUOT,
+     LCTL_T(KC_ESC), LSFT_T(KC_A), LT(SYMBOL, KC_S),    KC_D,    KC_F,   KC_G,                KC_H,    KC_J,    KC_K,    KC_L,    RSFT_T(KC_SCLN), RCTL_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, LT(ARROW, KC_Z), KC_X, KC_C, KC_V, KC_B, _______,              _______,  KC_N,    LT(MEDIA, KC_M),    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
