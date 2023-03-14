50a78aab232edfe2a0409d74027f8cdab80338a7
Parker Smith <parker@parkersmith.us>
2023-03-13 20:28:01 -0600

    Change hyper mod tap key

--- keyboards/keebio/iris/keymaps/divsmith/keymap.c
+++ keyboards/keebio/iris/keymaps/divsmith/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, LSFT_T(KC_A), KC_S, LT(ARROW, KC_D), KC_F,   KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    RSFT_T(KC_SCLN), RCTL_T(KC_QUOT),
+     KC_LCTL, LSFT_T(KC_A), KC_S, LT(ARROW, KC_D), KC_F,   KC_G,                      KC_H,    KC_J,    KC_K,    MT(MOD_HYPR, KC_L), RSFT_T(KC_SCLN), RCTL_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, LOPT_T(KC_Z), KC_X, KC_C, KC_V, KC_B, _______,                  _______,  KC_N,    LT(MEDIA, KC_M),    KC_COMM, KC_DOT,  ROPT_T(KC_SLSH), KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
