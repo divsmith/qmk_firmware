30dde93177ba95be724085e0bf2387ee60609f1e
Parker Smith <parker@parkersmith.us>
2023-03-19 21:33:46 -0600

    move layer switches

--- keyboards/keebio/iris/keymaps/divsmith/keymap.c
+++ keyboards/keebio/iris/keymaps/divsmith/keymap.c
@@ -42,13 +42,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_TAB,  KC_Q,    KC_W,    LT(NAV, KC_E),    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, LSFT_T(KC_A), KC_S, KC_D, LGUI_T(KC_F),   KC_G,                         KC_H,    RGUI_T(KC_J), KC_K, KC_L, TD(SCLN_CLN_SHFT), RCTL_T(KC_QUOT),
+     KC_LCTL, LSFT_T(KC_A), KC_S, KC_D, LGUI_T(KC_F), KC_G,                           KC_H,    RGUI_T(KC_J), KC_K, KC_L, TD(SCLN_CLN_SHFT), RCTL_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, LOPT_T(KC_Z), KC_X, KC_C, LT(ARROW, KC_V), KC_B, TG(ARROW),      _______,  KC_N,    LT(MEDIA, KC_M),    KC_COMM, KC_DOT,  ROPT_T(KC_SLSH), KC_RSFT,
+     KC_LSFT, LOPT_T(KC_Z), KC_X, KC_C, LT(ARROW, KC_V), KC_B, _______,      _______,  KC_N,    LT(MEDIA, KC_M),    KC_COMM, KC_DOT,  ROPT_T(KC_SLSH), KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, LT(SYMBOL, KC_ESC), MO(NAV),        MT(MOD_HYPR, KC_SPC), KC_ENT, _______
+                                    _______, LT(SYMBOL, KC_ESC), _______,        MT(MOD_HYPR, KC_SPC), KC_ENT, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
   
