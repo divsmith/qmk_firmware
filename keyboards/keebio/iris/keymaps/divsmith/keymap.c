a1a00a9b1dda3b33c6bdf523aa19bcc006a16dae
Parker Smith <parker@parkersmith.us>
2023-03-19 22:04:15 -0600

    layer shift stuff here now

--- keyboards/keebio/iris/keymaps/divsmith/keymap.c
+++ keyboards/keebio/iris/keymaps/divsmith/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    LT(NAV, KC_E),    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, LSFT_T(KC_A), KC_S, KC_D, LGUI_T(KC_F), KC_G,                           KC_H,    RGUI_T(KC_J), KC_K, KC_L, TD(SCLN_CLN_SHFT), RCTL_T(KC_QUOT),
+     KC_LCTL, LSFT_T(KC_A), KC_S, KC_D, LGUI_T(KC_F), KC_G,                           KC_H,    RGUI_T(KC_J), LT(SYMBOL, KC_K), KC_L, TD(SCLN_CLN_SHFT), RCTL_T(KC_QUOT),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, LOPT_T(KC_Z), KC_X, KC_C, LT(ARROW, KC_V), KC_B, _______,      _______,  KC_N,    LT(MEDIA, KC_M),    KC_COMM, KC_DOT,  ROPT_T(KC_SLSH), KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
